c3e69a21adbb9b8821e6805c02c01b1cff455fb0
Dean <deancs1997@gmail.com>
2020-11-30 16:45:51 -0500

    Added new files and implementation
    Added docker compose to run mongodb
    added new class to run the mapreduce
    can now store data in db

--- src/service/StoreData.java
+++ src/service/StoreData.java
@@ -1,18 +1,29 @@
 package service;
 
-import java.io.*;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import com.mongodb.MongoClient;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StoreData {
     private static String fileUrl = "https://raw.githubusercontent.com/haniehalipour/Online-Machine-Learning-for-Cloud-Resource-Provisioning-of-Microservice-Backend-Systems/master/Workload%20Data/DVD-training.csv";
     private static BufferedReader br = null;
-    MongoClient mongoClient;
+    private static MongoClient mongoClient;
+
     public static void storeData() {
         String line = "";
         String byComma = ",";
+        List<Document> dataSets = new ArrayList<>();
 
         try {
             URL url = new URL(fileUrl);
@@ -20,16 +31,24 @@ public class StoreData {
 
             urlConnection.setRequestProperty("X-Requested-With", "Curl");
 
-            File csvFile;
             br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            //Skip the header
+            br.readLine();
+            long id = 0;
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
+                Document document = new Document();
                 String[] lines = line.split(byComma);
+                document.put("_id", id++);
+                document.put("cpuUsage", Integer.parseInt(lines[0]));
+                document.put("networkIn", Integer.parseInt(lines[1]));
+                document.put("networkOut", Integer.parseInt(lines[2]));
+                document.put("memoryUtilization", Float.parseFloat(lines[3]));
 
-                System.out.println("Data [cpu= " + lines[0] + " , name=" + lines[1] + "]");
-
+                dataSets.add(document);
             }
+            storeDataInDb(dataSets);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,4 +63,15 @@ public class StoreData {
             }
         }
     }
+
+    private static void storeDataInDb(List<Document> dataSets) {
+
+        MongoClientURI uri = new MongoClientURI("mongodb://root:root@localhost:27017/?authSource=admin");
+        mongoClient = new MongoClient(uri);
+        MongoDatabase db = mongoClient.getDatabase("DataSets");
+        MongoCollection<Document> documentMongoCollection = db.getCollection("dvd");
+
+        documentMongoCollection.insertMany(dataSets);
+    }
+
 }
